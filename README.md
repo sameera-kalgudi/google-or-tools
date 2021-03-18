55ce9820686b786a1ca6c9a62cb6e7a59f9c0189
sameera <sk1234@wildcats.unh.edu>
2021-03-18 15:25:25 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,10 +1,62 @@
-#Google OR - Indivisual study
+# Google OR - Indivisual study
 
 OR-Tools is an open source software suite for optimization, tuned for tackling the world's toughest problems. Will be learning how to implement the OR-Tools provided by google to solve some of the complex problems.
 
-##Installation 
+## Installation 
 - Python 3.9 is recomended.
-- Installation of ortools library using pipenv
-``` pipenv install ortools ```
-- Activate the virtual environment
-``` pipenv shell ```
+- Installation of ortools library using pipenv 
+    - ` pipenv install ortools ` 
+
+- Activate the virtual environment 
+    - ` pipenv shell `
+
+## 1.GLOP Solver
+GLOP linear computational optimization problems are solved using the Google OR-Tools linear optimization solver.
+Let us understand from the below example.
+
+- #### linear.py 
+  Here's the solution to a sample example of a linear programming problem from google developers website.
+
+  - **Problem Statement**<br />
+    Maximize 3x + 4y subject to the following constraints:<br />
+    x + 2y	≤	14<br />
+    3x – y	≥	0<br />
+    x – y	≤	2<br />
+
+  - ##### Execution
+    `python linear.py `
+
+## 2.Routing Solver
+Here we are trying to solve **Traveling Salesman Problem** using the Google OR-Tools routing model.
+Let us understand from the below example.
+
+- ##### routing.py 
+  Here's the solution for the traveling salesman problem from google developers website.
+
+  - **Poblem Statement**<br />
+    The senario is to find out the minimum cost and path travelled between these cities, such a way that it begins and ends from the same city.<br />
+  - **Given** <br />
+    '''0. New York - 1. Los Angeles - 2. Chicago - 3. Minneapolis - 4. Denver - 5. Dallas - 6. Seattle - 7. Boston - 8. San Francisco - 9. St. Louis - 10. Houston - 11. Phoenix - 12. Salt Lake City''' <br />
+
+  - ##### Execution
+    `python routing.py`
+
+- ##### routing_prob1.py
+  - **Problem Statement**<br />
+
+## 3.Scheduling Solver
+Here we are trying to solve shift scheduling using the Google OR-Tools scheduling model.
+Let us understand from the below example.
+
+- ##### scheduling.py
+  Here's the solution for the shift scheduling of the four nurses over a three-day period, subject to the following conditions:<br />
+
+    - Each day is divided into three 8-hour shifts.
+    - Every day, each shift is assigned to a single nurse, and no nurse works more than one shift.
+    - Each nurse is assigned to at least two shifts during the three-day period.
+
+
+
+
+
+
---
+++ routing_prob1.py
@@ -0,0 +1,77 @@
+"""Simple travelling salesman problem between cities."""
+
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
+def create_data_model():
+    """Stores the data for the problem."""
+    data = {}
+    data['distance_matrix'] = [
+        [0, 170, 290, 250, 230],
+        [170, 0, 240, 310, 280],
+        [290, 240, 0, 350, 420],
+        [250, 310, 250, 0, 210],
+        [230, 280, 420, 210, 0]
+    ]  # yapf: disable
+    data['num_vehicles'] = 1
+    data['depot'] = 0
+    return data
+
+
+def print_solution(manager, routing, solution):
+    """Prints solution on console."""
+    print('Objective: {} miles'.format(solution.ObjectiveValue()))
+    index = routing.Start(0)
+    plan_output = 'Route for vehicle 0:\n'
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += ' {} ->'.format(manager.IndexToNode(index))
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan_output += ' {}\n'.format(manager.IndexToNode(index))
+    print(plan_output)
+    plan_output += 'Route distance: {}miles\n'.format(route_distance)
+
+
+def main():
+    """Entry point of the program."""
+    # Instantiate the data problem.
+    data = create_data_model()
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(manager, routing, solution)
+
+
+if __name__ == '__main__':
+    main()
